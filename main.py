e99200a1d86e48e9c2808830bb0e6476eedb6dbe
Anas Khan <123714177+anasAnonymous@users.noreply.github.com>
2024-05-12 14:20:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -21,17 +21,16 @@ Libraries:
 """
 
 import sqlite3
-import secrets
-import random
+# import secrets
+# import random
 # import zxcvbn
-import cryptography
-import pyotp
-
+# import cryptography
+# import pyotp
 
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-
-
+import pyaes    # for AES
+# https://stackoverflow.com/questions/25261647/python-aes-encryption-without-extra-module
 
 db_connection = sqlite3.connect("passwords_DB.db")
 
@@ -44,36 +43,136 @@ def database_and_tables():
     );
     """)
 
+    # db_connection.execute("DROP TABLE credentials")
+
     db_connection.execute("""CREATE TABLE IF NOT EXISTS credentials(
+                 master_username VARCHAR(30) NOT NULL,
                  username_or_email VARCHAR(30) NOT NULL,  
                  encrypted_pass TEXT,  
                  website TEXT,  
                  website_URL TEXT,
-                 CONSTRAINT pk_constraint PRIMARY KEY (username_or_email, website)         
+                 CONSTRAINT pk_constraint PRIMARY KEY (master_username, website),
+                 FOREIGN KEY (master_username) REFERENCES master(username)         
     );
     """)
+
     db_connection.close()
 
 
+def program_menu(mt_username, enc_dec_key):
+    print("""Enter '1' to Add a Password
+Enter '2' to Delete a Password
+Enter '3' to Update your Password
+Enter '4' to View your Passwords
+Enter '5' to exit
+""")
+    choice = input("Enter your Choice : ")
+    if choice == '1':
+        add_password(mt_username, enc_dec_key)
+    elif choice == '2':
+        delete_password(mt_username)
+    elif choice == '3':
+        update_password(mt_username, enc_dec_key)
+    elif choice == '4':
+        view_password(mt_username, enc_dec_key)
+    elif choice == '5':
+        return
+    else:
+        print("Invalid Input!")
+        program_menu()
 
-def add_password():
-    print("add password")
+
+def add_password(username, enc_dec_key):
+    # print("add password")
+    website = input("Enter the Website name : ").lower()
+    website_URL = input("Enter the Website URL : ").lower()
+    username_or_email = input("Enter your username or email for the said website: ")
+    password = input("Enter the Password : ")
+
+    # wanna analyze password strength?      -->     strength()
+    aes = pyaes.AESModeOfOperationCTR(enc_dec_key)
+    enc_pass = aes.encrypt(password)
+    # print(enc_pass)
+
+    query = "INSERT INTO credentials (master_username, username_or_email, encrypted_pass, website, website_URL) VALUES(?,?,?,?,?)"
+    db_connection.execute(query, (username, username_or_email, enc_pass, website, website_URL))
+    db_connection.commit()
 
 
-def delete_password():
-    print("delete password")
+def delete_password(mt_username):
+    # print("delete password")
+    web = input("Enter the Website you want to delete your Credentials for : ").lower()
+    
+    query_for_fetching = "SELECT * FROM credentials WHERE website = ? AND master_username = ?"
+    cursor = db_connection.cursor()
+
+    try:
+        cursor.execute(query_for_fetching, (web, mt_username))
+        record = cursor.fetchone()
+        if record is None:
+            print("No Record Found!")
+        else:
+            query = "DELETE FROM credentials WHERE website = ? AND master_username = ?"
+            db_connection.execute(query, (web, mt_username))
+            db_connection.commit()
+            print(f"Record for website '{web}' has been deleted successfully!")
+
+    except sqlite3.Error as error:
+        print("Error while Deleting Credentials! : ", error)
+    finally:
+        cursor.close()
 
 
-def update_password():
-    print("update password")
+def update_password(mt_username, enc_dec_key):
+    # print("update password")
+    web = input("Enter the Website you want to Update your Credentials for : ").lower()
+
+    while True:
+        new_password = input("Enter the new password : ")
+        verify_pass = input("Enter the new password again : ")
+        if new_password != verify_pass:
+            print("Password does not match! \n")
+            continue
+        else:
+            # print("on")
+            #update logic
+            aes_algo = pyaes.AESModeOfOperationCTR(enc_dec_key)
+            enc_pass = aes_algo.encrypt(new_password)
+            query = "UPDATE credentials SET encrypted_pass = ? WHERE website = ? AND master_username = ?"
+            db_connection.execute(query, (enc_pass, web, mt_username))
+            db_connection.commit()
+            #   wanna analyze password strength?      -->     strength()
+            break
 
 
-def view_password():
-    print("view password")
+def view_password(mt_username, enc_dec_key):
+    # print("view password")
+    query = "SELECT * FROM credentials WHERE master_username = ?"
+    cursor = db_connection.cursor()
+
+    try:
+        cursor.execute(query, (mt_username,))
+        records = cursor.fetchall()
+        if records is None:
+            print("No Record Found!")
+        else:
+            for record in records:
+                enc_pass = record[2]
+                aes = pyaes.AESModeOfOperationCTR(enc_dec_key)
+                decrypted_password = aes.decrypt(enc_pass).decode('utf-8')
+                print("-" * 30)
+                print(f"Master Username : {record[0]}") 
+                print(f"Username/Email associated with the website : {record[1]}")
+                print(f"Password : {decrypted_password}")
+                print(f"Webiste : {record[3]}")
+                print(f"Website URL : {record[4]}")
+    except sqlite3.Error as error:
+        print("Error while viewing informaton! : ", error)
+    finally:
+        cursor.close()
 
 
 def kdf(master_password, salt_type):
-    # salt_string = "unpredictable"
     salt_bytes = salt_type.encode() 
 
     kdf = PBKDF2HMAC(
@@ -93,19 +192,21 @@ def insert_into_master(username, verification_key):
     db_connection.commit()
 
 
-def verification(username, verification_key):
+def verification(username, verification_key, encryption_key):
     query = "SELECT verification_key FROM master WHERE username = ?"
     cursor = db_connection.cursor()
 
     try:
         cursor.execute(query, (username,))
         record = cursor.fetchone()
-        print(record[0])
-        if verification_key == record[0]:
-            print("vrerified")
+        if record is None or verification_key != record[0]:
+                print("Invalid username or password!")
+                sign_in()
         else:
-            print("Unauthorized")
-        return record
+            print("verified")
+            program_menu(username, encryption_key)
+            # print(record[0])
+            # return record[0]
     except sqlite3.Error as error:
         print("Error while verification : ", error)
         return None
@@ -113,50 +214,56 @@ def verification(username, verification_key):
         cursor.close()
 
 
-
 def sign_up():
-    username = input("Enter a username (MUST BE UNIQUE) : ")
-    #check if alr exist (DB)
+    while True:
+        username = input("Enter a username (MUST BE UNIQUE) : ")
+        #check if alr exists (DB)
+        query = "SELECT username FROM master"
+        cursor = db_connection.cursor()
+        cursor.execute(query)
+        usernames_fetched = cursor.fetchall()
 
-    master_password = input("Enter a strong master password (specify characteristics): ")
-    #strength condition verify 
+        exists = False
+        for user in usernames_fetched:
+            if user[0] == username:
+                exists = True
+                print("Username already exists! ")
+                break
+        if not  exists:
+            master_password = input("Enter a strong master password (specify characteristics): ")
+            #strength condition verify 
 
-    verification_salt = "salt_4_ver"
-    verification_key = kdf(master_password, verification_salt)
-    # print(verification_key)
-    insert_into_master(username, verification_key)
-    # verification(username, verification_key)
-
-    encryption_salt = "salt_4_enc"
-    encryption_key = kdf(master_password, encryption_salt)
-    # print(encryption_key)
+            verification_salt = "salt_4_ver"
+            verification_key = kdf(master_password, verification_salt)
+            # print(verification_key)
+            insert_into_master(username, verification_key)
+            return
 
 
 def sign_in():
     username = input("Enter your username : ")
-    #check if alr exist (DB)
 
     master_password = input("Enter your master password (acc to the specified characteristics): ")
     #strength condition verify 
 
-    verification_salt = "salt_4_ver"
-    verification_key = kdf(master_password, verification_salt)
-    print(verification_key)
-    # insert_into_master(username, verification_key)
-    verification(username, verification_key)
-
     encryption_salt = "salt_4_enc"
     encryption_key = kdf(master_password, encryption_salt)
     # print(encryption_key)
+
+    verification_salt = "salt_4_ver"
+    verification_key = kdf(master_password, verification_salt)
+    # print(verification_key)
+    verification(username, verification_key, encryption_key)
 
 
 
 def main():
     print("main")
     # database_and_tables()
-    sign_up()
-    # sign_in()
-
+    # sign_up()
+    sign_in()
+    # program_menu()
+    # update_password("mt", "enc")
 
     # LASTTTTTTTTTTTTTTT
     db_connection.close()
